7860b517903f6863986b5cba2207850f5414b959
YashrajMalviya <yashrajmalviya296@gmail.com>
2023-10-26 01:00:22 +0530

    layout completed

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -8,12 +8,39 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: const Color(0xff757575),
       child: Container(
+        padding: const EdgeInsets.all(20),
         decoration: const BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.only(
               topRight: Radius.circular(20),
               topLeft: Radius.circular(20),
             )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Text("Add Task",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 20, color: Colors.lightBlueAccent)),
+            const TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                  fixedSize: const Size(300, 50),
+                  elevation: 12.0,
+                  textStyle: const TextStyle(color: Colors.white)),
+              child: const Text(
+                'Add',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
