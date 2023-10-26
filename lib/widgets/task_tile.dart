75dbfef74203d098bbed6285d21b2989c5d90ee4
YashrajMalviya <yashrajmalviya296@gmail.com>
2023-10-26 15:08:46 +0530

    Global state - sync checkbox tick & linethrough

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
   const TaskTile({
     super.key,
   });
 
   @override
+  State<TaskTile> createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-        title: const Text("Task is completed"),
-        trailing: Checkbox(value: false, onChanged: (bool? value) {  })
+        title: Text(
+          "Task is completed",
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: TaskCheckBox(isChecked, (bool? checkBoxState) {
+          setState(() {
+            isChecked = checkBoxState!;
+          });
+        }));
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final void Function(bool?)? toggleCheckBoxState;
+  const TaskCheckBox(this.checkBoxState, this.toggleCheckBoxState, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
