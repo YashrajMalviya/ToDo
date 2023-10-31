43389c3e63ea8be52ae017e12adafbde2242c1ef
YashrajMalviya <yashrajmalviya296@gmail.com>
2023-10-31 19:09:18 +0530

    listView.Builder implemented & managing state by passing callback from child to parent for onchanged

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_flutter/widgets/task_tile.dart';
+import 'package:to_do_flutter/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
   @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: "Complete Physics hw"),
+    Task(name: "Complete Chemistry hw"),
+    Task(name: "Complete Math hw"),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const [
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTitle: tasks[index].name,
+          isChecked: tasks[index].isDone,
+          checkboxCallback: (checkBoxState) {
+            setState(
+              () {
+                tasks[index].toggleDone();
+              },
+            );
+          },
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
