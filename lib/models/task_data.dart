9833ac16d13d461cca14b8afe15918a4bbcea270
YashrajMalviya <yashrajmalviya296@gmail.com>
2023-11-05 13:23:34 +0530

    model created for tracking changes

---
+++ lib/models/task_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/foundation.dart';
+import 'task.dart';
+
+class TaskData extends ChangeNotifier{
+  List<Task> tasks = [
+    Task(name: "Complete Physics hw"),
+    Task(name: "Complete Chemistry hw"),
+    Task(name: "Complete Math hw"),
+  ];
+}
